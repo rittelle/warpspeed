37f4e2090652128263c3be535d05ec6e3783533b
rittelle <33598633+rittelle@users.noreply.github.com>
2023-03-26 23:57:55 +0200

    Implement text highlighting
    Implement basic highlighting of text fragments which will later be used to show matches.

--- src/main/kotlin/WarpSpeedExtension.kt
+++ src/main/kotlin/WarpSpeedExtension.kt
@@ -1,12 +1,17 @@
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.markup.*
 import com.maddyhome.idea.vim.command.MappingMode
 import com.maddyhome.idea.vim.extension.VimExtension
 import com.maddyhome.idea.vim.extension.VimExtensionFacade
 import com.maddyhome.idea.vim.extension.VimExtensionHandler
 import com.maddyhome.idea.vim.helper.StringHelper
+import java.awt.Font
 
 class WarpSpeedExtension : VimExtension {
+    private val markHandler = MarkHandler()
+
     override fun getName(): String = "warpspeed"
 
     override fun init() {
@@ -14,7 +19,7 @@ class WarpSpeedExtension : VimExtension {
             MappingMode.NXO,
             StringHelper.parseKeys("<Plug>(warpspeed-s)"),
             owner,
-            WarpSpeedExtensionHandler(),
+            WarpSpeedExtensionHandler(markHandler),
             false
         )
         VimExtensionFacade.putKeyMapping(
@@ -26,9 +31,47 @@ class WarpSpeedExtension : VimExtension {
         )
     }
 
-    private class WarpSpeedExtensionHandler : VimExtensionHandler {
+    private class WarpSpeedExtensionHandler(val markHandler: MarkHandler) : VimExtensionHandler {
         override fun execute(editor: Editor, context: DataContext) {
-            TODO("Not yet implemented")
+            markHandler.addMark(
+                editor,
+                editor.caretModel.primaryCaret.offset..editor.caretModel.primaryCaret.offset + 2, "?"
+            )
         }
+    }
+
+    /**
+     * A class to manage the marks shown when the plugin is used.
+     *
+     * A mark consists of an highlighlight of the matched text and a label showing the shortcut.
+     */
+    private class MarkHandler {
+        private class Mark(val editor: Editor, val highlight: RangeHighlighter)
+
+        private val labels = mutableSetOf<Mark>()
+
+        fun addMark(editor: Editor, range: ClosedRange<Int>, label: String) {
+            labels.add(
+                Mark(
+                    editor,
+                    editor.markupModel.addRangeHighlighter(
+                        range.start,
+                        range.endInclusive,
+                        HighlighterLayer.SELECTION,
+                        getHighlightAttributes(editor),
+                        HighlighterTargetArea.EXACT_RANGE
+                    )
+                )
+            )
+            // TODO: draw label after the highlighted text
+        }
+
+        private fun getHighlightAttributes(editor: Editor) = TextAttributes(
+            null,
+            EditorColors.TEXT_SEARCH_RESULT_ATTRIBUTES.defaultAttributes.backgroundColor,
+            editor.colorsScheme.getColor(EditorColors.CARET_COLOR),
+            EffectType.SEARCH_MATCH,
+            Font.PLAIN
+        )
     }
 }
