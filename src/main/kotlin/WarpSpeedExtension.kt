a6f2022f794d56e87d8cd2a7c81545d501697486
rittelle <33598633+rittelle@users.noreply.github.com>
2023-04-20 03:42:01 +0200

    Add a test overlay
    Add some code that draws a box and some text for each marker.  Currently, only test output is created, but this will be used for proper labels in a later commit.

--- src/main/kotlin/WarpSpeedExtension.kt
+++ src/main/kotlin/WarpSpeedExtension.kt
@@ -2,12 +2,17 @@ import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.colors.EditorColors
 import com.intellij.openapi.editor.markup.*
+import com.intellij.ui.JBColor
 import com.maddyhome.idea.vim.command.MappingMode
 import com.maddyhome.idea.vim.extension.VimExtension
 import com.maddyhome.idea.vim.extension.VimExtensionFacade
 import com.maddyhome.idea.vim.extension.VimExtensionHandler
 import com.maddyhome.idea.vim.helper.StringHelper
 import java.awt.Font
+import java.awt.Graphics
+import java.awt.Point
+import java.awt.Rectangle
+import javax.swing.JComponent
 
 class WarpSpeedExtension : VimExtension {
     private val markHandler = MarkHandler()
@@ -46,24 +51,60 @@ class WarpSpeedExtension : VimExtension {
      * A mark consists of an highlighlight of the matched text and a label showing the shortcut.
      */
     private class MarkHandler {
-        private class Mark(val editor: Editor, val highlight: RangeHighlighter)
+        private class Mark(val editor: Editor, val highlight: RangeHighlighter, val label: String) : JComponent() {
+            init {
+                val contentComponent = editor.contentComponent
+                contentComponent.add(this)
+                setBounds(0, 0, contentComponent.width, contentComponent.height)
+            }
+
+            private fun getRectangle(): Rectangle {
+                val startXY = editor.offsetToXY(highlight.startOffset)
+                val endXY = editor.offsetToXY(highlight.endOffset)
+                return Rectangle(
+                    startXY.x,
+                    startXY.y,
+                    endXY.x - startXY.x,
+                    endXY.y - startXY.y + 20
+                )
+            }
+
+            private fun drawBackground(g: Graphics, rect: Rectangle) {
+                val radius = 5
+                g.color = JBColor.RED
+                g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, radius, radius)
+            }
+
+            private fun drawText(g: Graphics, point: Point, text: String) {
+                g.color = JBColor.GREEN
+                g.font = Font("Arial", Font.BOLD, 20)
+                g.drawString(text, point.x, point.y)
+            }
+
+            override fun paint(g: Graphics) {
+                super.paint(g)
+                val rect = getRectangle()
+                drawBackground(g, rect)
+                drawText(g, Point(rect.x, rect.y), label)
+            }
+        }
 
         private val labels = mutableSetOf<Mark>()
 
         fun addMark(editor: Editor, range: ClosedRange<Int>, label: String) {
-            labels.add(
-                Mark(
-                    editor,
-                    editor.markupModel.addRangeHighlighter(
-                        range.start,
-                        range.endInclusive,
-                        HighlighterLayer.SELECTION,
-                        getHighlightAttributes(editor),
-                        HighlighterTargetArea.EXACT_RANGE
-                    )
-                )
+            val mark = Mark(
+                editor,
+                editor.markupModel.addRangeHighlighter(
+                    range.start,
+                    range.endInclusive,
+                    HighlighterLayer.SELECTION,
+                    getHighlightAttributes(editor),
+                    HighlighterTargetArea.EXACT_RANGE
+                ),
+                label
             )
-            // TODO: draw label after the highlighted text
+            labels.add(mark)
+            mark.repaint()
         }
 
         private fun getHighlightAttributes(editor: Editor) = TextAttributes(
